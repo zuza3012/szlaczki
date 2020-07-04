bca05d9e8bcb836f8d05d7799defde05ac9e7bed
zuza3012 <zuza3012@github.com>
2020-07-04 19:12:05 +0200

    nie dzialajaca kupcia

---
+++ make_exec.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+   
+current_path=$PWD
+build_dest="$current_path/build"
+if [ ! -d $build_dest ]; then
+    mkdir -p $build_dest
+    echo "$current_path"
+    echo "Build folder created"
+else
+    echo "Build folder already exists." 
+    exit 9999 # die with error code 9999
+fi
+
+add="/CMakeLists.txt"
+file="$current_dest$add"
+
+if [ -e "$file" ]; then
+    echo "CMakeLists.txt exists"
+    exit 9999
+else 
+    echo "CMakeLists.txt does not exist"
+    echo "$file"   
+fi  
+  
+  
+~                                                                                                    
+~                                                                                                    
+~                                                                                                    
+~                 
